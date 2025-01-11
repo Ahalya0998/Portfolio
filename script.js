62901c48daeaa16e5d72798e577a13eff615915c
Ahalya0998 <ahalyakaranth09@gmail.com>
2025-01-11 17:13:06 +0530

    updated codes

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-// Function to show only the clicked section
+
 function showSection(sectionId) {
-    // Hide all sections
+  
     const sections = document.querySelectorAll('.content');
     sections.forEach(section => {
         section.style.display = 'none';
     });
 
-    // Show the selected section
+  
     const selectedSection = document.getElementById(sectionId);
     selectedSection.style.display = 'block';
 }
 
-// Initially, show the home section by default
+
 window.onload = function() {
     showSection('home');
 };
+
+
+
+
+
+
+
+
+
 
 
